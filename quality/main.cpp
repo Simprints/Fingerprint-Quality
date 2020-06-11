ef482dbc0dd5043c2453c3c8bb131c1443095316
Samad <samad@simprints.com>
2020-06-11 12:25:02 +0100

    Adding WSQ lib

--- quality/main.cpp
+++ quality/main.cpp
@@ -3,34 +3,33 @@
 #include "SecugenWrapper.h"
 #include "FileWrapper.h"
 #include "GSUtilWrapper.h"
-
-#include <fstream>
-#include <string>
-#include <iostream>
-void write()
-{
-	std::string input("hello");
-	std::ofstream out("output.txt");
-	out << input;
-	out.close();
-
+extern "C" {
+#include "lib/wsq/include/wsq.h"
 }
 
-int main()
-{
-	// Stage 1: get fingerprint urls into txt file
+void Test_SecugenFingerprintQuality() {
 	FileWrapper files;
 	SecugenWrapper secugen;
 	assert(secugen.GetQuality(files.readFile("qualityscore19.raw").data()) == 19);
 	assert(secugen.GetQuality(files.readFile("qualityscore59.raw").data()) == 59);
 	assert(secugen.GetQuality(files.readFile("qualityscore99.raw").data()) == 99);
+}
+
+void Stage1_CollectFingerprintLists() {
+	// Stage 1: get fingerprint urls into txt file
+	FileWrapper files;
 
 	GsutilWrapper gsutil;
 	gsutil.SetBucket("simprints-152315-images-eu");
 	gsutil.SetProject("fUBnpzDdbsCsMp0egCHB");
-	
-	std::string urls = gsutil.ListFingerprintImages();	
+
+	std::string urls = gsutil.ListFingerprintImages();
 	files.writeFile("fingerprintslist.txt", urls);
+}
+
+int main()
+{
+	Stage1_CollectFingerprintLists();
 
 	// Stage 2: read txt file line by line and download fingerprint, get quality, save to csv file {url, quality} 
 
